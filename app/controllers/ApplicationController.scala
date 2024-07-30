fd276cd9addbe145fbd0f0a00d761d3cdd6ff71c
Arei Mohammed <Arei1997@hotmail.co.uk>
2024-07-30 13:49:10 +0100

    dqata respository cleaning

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,16 +1,19 @@
 package controllers
+
+import repositories.repositories.DataRepository
 import play.api.mvc.{BaseController, ControllerComponents}
+
 import javax.inject.Inject
 import javax.inject._
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
-  def index() = TODO
-  def create() = TODO
-
-  def read(id: String) = Action {
-    Ok("hi im arei")
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents,val dataRepository: DataRepository,implicit val ec: ExecutionContext) extends BaseController{
+  def index()  = Action { implicit request =>
+    Ok("Welcome to the Areis Play Scala API")
   }
+  def create() = TODO
+  def read(id: String) = TODO
   def update(id: String) = TODO
   def delete(id: String) = TODO
 }
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,16 +1,27 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers._
+import scala.concurrent.ExecutionContext
+import repositories.repositories.DataRepository
 
 class ApplicationControllerSpec extends BaseSpecWithApplication{
 
   val TestApplicationController = new ApplicationController(
-    component
+    component,
+    repository,
+    executionContext
   )
 
-  "ApplicationController .index()" should {
+  "ApplicationController .index" should {
 
-  }
+    "return 200 OK and a welcome message" in {
+      val result = TestApplicationController.index()(FakeRequest())
+      status(result) shouldBe Status.OK
+      contentAsString(result) should include ("Welcome to the Areis Play Scala API")
+    }
 
   "ApplicationController .create()" should {
 
@@ -28,3 +39,5 @@ class ApplicationControllerSpec extends BaseSpecWithApplication{
   }
 
 }
+}
+
