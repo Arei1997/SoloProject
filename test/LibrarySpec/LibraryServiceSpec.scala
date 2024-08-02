176969be233ad37c545a2cb6d53d86252a6e80e7
Arei Mohammed <Arei1997@hotmail.co.uk>
2024-08-02 13:20:06 +0100

    Renamed libraryserviced and created test spec

---
+++ test/LibrarySpec/LibraryServiceSpec.scala
@@ -0,0 +1,11 @@
+package LibrarySpec
+
+import baseSpec.BaseSpec
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures with GuiceOneAppPerSuite{
+
+
+}
